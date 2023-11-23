cdd22c9b5130b37b431aa550b5a822089137110c
黄芬达 <huangfenda@huangfendadeMac-mini.local>
2023-11-23 22:23:24 +0800

    add viper config

--- db/sqlc/entry_test.go
+++ db/sqlc/entry_test.go
@@ -6,7 +6,7 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/require"
-	"github.com/techschool/simplebank/util"
+	"github.com/gongjunhuang/go/util"
 )
 
 func createRandomEntry(t *testing.T, account Account) Entry {
